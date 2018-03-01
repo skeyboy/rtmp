752bfc3ed8bf0034159cd8492d68c8f16d7fda5e
le <le@ledeMacBook-Pro.local>
2018-03-01 11:01:43 +0800

    修改jdbc连接， 实现 play publish的监听

--- src/xsk/rtmp/controller/RTMPController.java
+++ src/xsk/rtmp/controller/RTMPController.java
@@ -1,5 +1,10 @@
 package xsk.rtmp.controller;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import xsk.rtmp.DBUtil;
@@ -7,111 +12,116 @@ import xsk.rtmp.DBUtil;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.*;
+import java.util.*;
 
 @Controller
 public class RTMPController {
-    Connection connection = DBUtil.getConnection();
-
+    JDBC jdbc = new JDBC();
     @RequestMapping(value = "/publish", method = RequestMethod.POST)
     @ResponseBody
-//    public String record_done(@RequestParam String call, @RequestParam String recorder, @RequestParam String name, @RequestParam String tcurl, @RequestParam String path, @RequestParam String addr, @RequestParam String app, @RequestParam int bytes_in, @RequestParam int clientid, @RequestParam int bytes_out) {
     public String publish(@RequestBody String params) throws SQLException {
-        String[] items = params.split("&");
-        Map<String, String> map = new HashMap<>();
-        for (String item : items) {
-            String[] comps = item.split("=");
-            String key = comps[0];
-            String value = comps[1];
-            map.put(key, value);
-        }
-        try {
-            PrintWriter pw = new PrintWriter(new FileWriter("publish.txt"));
-            pw.append(map.toString());
-
-            pw.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        System.out.println(params);
+        jdbc.add("publish", params, new String[]{"call"});
         return "index";
     }
 
     @RequestMapping(value = "/play", method = RequestMethod.POST)
     @ResponseBody
-//    public String record_done(@RequestParam String call, @RequestParam String recorder, @RequestParam String name, @RequestParam String tcurl, @RequestParam String path, @RequestParam String addr, @RequestParam String app, @RequestParam int bytes_in, @RequestParam int clientid, @RequestParam int bytes_out) {
     public String play(@RequestBody String params) throws SQLException {
-        String[] items = params.split("&");
-        Map<String, String> map = new HashMap<>();
-        for (String item : items) {
-            String[] comps = item.split("=");
-            String key = comps[0];
-            String value = comps[1];
-            map.put(key, value);
-        }
-        try {
-            PrintWriter pw = new PrintWriter(new FileWriter("play.txt"));
-            pw.append(map.toString());
-
-            pw.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        System.out.println(params);
+        jdbc.add("play", params, new String[]{"call"});
         return "index";
     }
 
     @RequestMapping(value = "/record_done", method = RequestMethod.POST)
     @ResponseBody
-//    public String record_done(@RequestParam String call, @RequestParam String recorder, @RequestParam String name, @RequestParam String tcurl, @RequestParam String path, @RequestParam String addr, @RequestParam String app, @RequestParam int bytes_in, @RequestParam int clientid, @RequestParam int bytes_out) {
     public String record_done(@RequestBody String params) throws SQLException {
 
-        String[] items = params.split("&");
-        Map<String, String> map = new HashMap<>();
-        for (String item : items) {
-            String[] comps = item.split("=");
-            String key = comps[0];
-            String value = comps[1];
-            map.put(key, value);
-        }
+
         try {
             PrintWriter pw = new PrintWriter(new FileWriter("record_done.txt"));
-            pw.append(map.toString());
+            pw.append(params);
 
             pw.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        try {
-            Statement statement = connection.createStatement();
-            String[] itemNames = new String[]{"call_path", "app", "path", "tcurl", "recorder", "clientid", "bytes_out", "bytes_in", "name", "addr"};
-            StringBuffer buffer = new StringBuffer();
-            for (String item : itemNames) {
-                buffer.append(item + " ,");
-            }
-            String sql = "insert into jiemu(" + buffer.substring(0, buffer.length() - 2) + ") values(?,?,?,?,?,?,?,?,?,?)";
-            PreparedStatement psmt = connection.prepareStatement(sql);
-            for (int i = 0; i < itemNames.length; i++) {
-                if (i == 0) {
 
-                    psmt.setObject(i + 1, map.get("call"));
 
-                    continue;
-                }
-                psmt.setObject(i + 1, map.get(itemNames[i]));
-            }
-            psmt.execute();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        jdbc.addJieMu(params);
 
         return "index";
+    }
+}
+
+class JDBC {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/TEST?useUnicode=true&characterEncoding=UTF-8";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "";
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private DriverManagerDataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    public JDBC() {
+        dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setPassword(PASSWORD);
+        dataSource.setUsername(NAME);
+        dataSource.setUrl(URL);
+
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+    }
+
+    public int addJieMu(String play_done) {
+
+        return add("jiemu", play_done, new String[]{"call"});
+    }
+
+    public int add(String tableName, String rtmp_response, String[] ignoreColus) {
+        List<String> keys = new ArrayList<>();
+        Map<String, Object> params = new HashMap<>();
+
+        String[] items = rtmp_response.split("&");
+        Map<String, String> map = new HashMap<>();
+        for (String item : items) {
+            String[] comps = item.split("=");
+            if (comps.length != 2) {
+                continue;
+            }
+            String key = comps[0];
+
+            if (Arrays.asList(ignoreColus).contains(key)) {
+                continue;
+            }
+            String value = comps[1];
+            params.put(key, value);
+            keys.add(key);
+        }
+
+
+        StringBuffer buffer = new StringBuffer();
+        StringBuffer valueBuffer = new StringBuffer();
+        for (String item : keys) {
+            buffer.append(item + " ,");
+            valueBuffer.append(":" + item + ",");
+        }
+        String sql = "insert into " + tableName + "(" + buffer.substring(0, buffer.length() - 2) + ") values(" + valueBuffer.substring(0, valueBuffer.length() - 1) + ")";
+        System.out.println(sql);
+        int result = -1;
+        try {
+            result = namedParameterJdbcTemplate.update(sql, params);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        } finally {
+            return result;
+        }
+
     }
 }
